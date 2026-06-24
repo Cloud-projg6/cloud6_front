a583dcd365679f2cb287c5ac621ed1a83e6e840b
hoppangm3 <hoppangm3@gmail.com>
2026-06-24 16:48:32 +0900

    chore: update backend endpoint to ALB URL

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'http://user53-alb-2060019532.us-west-1.elb.amazonaws.com';
 export const BOOKS_URL = `${BASE_URL}/books`;
 export const AUTH_URL = `${BASE_URL}/auth`;
