f2506b224d6094dbe5c2af2567537d3b70639cb9
surin <surinrtf@gmail.com>
2026-06-25 11:46:57 +0900

    fix error

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'http://user48-alb-973949459.us-west-1.elb.amazonaws.com';
 export const BOOKS_URL = `${BASE_URL}/books`;
 export const AUTH_URL = `${BASE_URL}/auth`;
