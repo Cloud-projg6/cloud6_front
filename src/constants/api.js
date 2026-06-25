1070e363766306926813d2b7f6d486286bfd49ec
Qlrl <kjminji01@gmail.com>
2026-06-25 09:36:23 +0900

    alb 주소 fix

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = 'http://localhost:8080';
-export const BASE_URL = 'http://alb-user40-team6-backend-419547333.us-east-2.elb.amazonaws.com/';
+export const BASE_URL = 'http://alb-user40-team6-backend-419547333.us-east-2.elb.amazonaws.com';
 export const BOOKS_URL = `${BASE_URL}/books`;
 export const AUTH_URL = `${BASE_URL}/auth`;
