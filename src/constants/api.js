6789c23ccd710fc8e933822589615f5e45c7916d
Qlrl <kjminji01@gmail.com>
2026-06-25 10:12:42 +0900

    BASE_URL을 프론트엔드가 구동 중인 CloudFront 주소(HTTPS)로 일치

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,6 @@
 // export const BASE_URL = 'http://localhost:8080';
-export const BASE_URL = 'http://alb-user40-team6-backend-419547333.us-east-2.elb.amazonaws.com';
+// export const BASE_URL = 'http://alb-user40-team6-backend-419547333.us-east-2.elb.amazonaws.com';
+// BASE_URL을 프론트엔드가 구동 중인 CloudFront 주소(HTTPS)로 일치
+export const BASE_URL = 'https://d913vpzid8uar.cloudfront.net'
 export const BOOKS_URL = `${BASE_URL}/books`;
 export const AUTH_URL = `${BASE_URL}/auth`;
